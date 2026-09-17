b3606a985a375050eeedbb7a7981aa4330763935
Abdullah Al Sakib <abdullah.sakib2825@gmail.com>
2026-09-17 22:53:57 +0600

    added a submodule spdog for logging system

--- The-Engine/src/Engine/EntryPoint.h
+++ The-Engine/src/Engine/EntryPoint.h
@@ -4,6 +4,13 @@
 	extern Engine::Application* Engine::CreateApplication();
 
 	int main(int argc, char** argv) {
+		Engine::Log::init();
+		
+		ENGINE_CORE_ERROR("Error on line 32!");
+		int a=5;
+		ENGINE_INFO("Solved var={0}",a);
+
+
 		auto app = Engine::CreateApplication();
 		app->Run();
 		delete app;
